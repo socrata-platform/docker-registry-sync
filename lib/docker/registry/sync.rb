38a4d4642223e8d1037ac00956773721f8cff6ff
Brian Oldfield <brian.oldfield@socrata.com>
2015-09-09 06:15:13 -0700

    Moving RingBuffer object to accessable location

--- lib/docker/registry/sync.rb
+++ lib/docker/registry/sync.rb
@@ -5,32 +5,32 @@ require 'docker/registry/sync/version'
 module Docker
   module Registry
     module Sync
+      class RingBuffer < Array
+        attr_reader :max_size
+
+        def initialize(max_size, enum = nil)
+          @max_size = max_size
+          enum.each { |e| self << e } if enum
+        end
+
+        def <<(el)
+          if self.size < @max_size || @max_size.nil?
+            super
+          else
+            self.shift
+            self.push(el)
+          end
+        end
+
+        alias :push :<<
+      end
+
       class << self
         attr_accessor :config
 
         def configure
           self.config ||= Docker::Registry::Sync::Configuration.new
           yield self.config
-        end
-
-        class RingBuffer < Array
-          attr_reader :max_size
-
-          def initialize(max_size, enum = nil)
-            @max_size = max_size
-            enum.each { |e| self << e } if enum
-          end
-
-          def <<(el)
-            if self.size < @max_size || @max_size.nil?
-              super
-            else
-              self.shift
-              self.push(el)
-            end
-          end
-
-          alias :push :<<
         end
       end
     end
